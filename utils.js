e4476a206cbe2c99b02deec3df4e8673df055f57
EYDUAR <128336023+EYDUAR@users.noreply.github.com>
2025-10-30 20:52:08 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T17:47:30.612Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-10-30T17:52:07.610Z
+Commit: Fix broken links in documentation
