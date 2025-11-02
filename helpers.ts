de74b92c9694f969828521988c151e901fd79911
EYDUAR <128336023+EYDUAR@users.noreply.github.com>
2025-11-02 12:04:04 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T17:52:39.862Z
-Commit: Fix race condition in async operations
+Timestamp: 2025-11-02T09:04:04.147Z
+Commit: Patch security vulnerability in authentication
