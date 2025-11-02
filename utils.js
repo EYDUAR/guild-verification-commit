e1240aa3f17e06653868780772d2883f1fcb622d
EYDUAR <128336023+EYDUAR@users.noreply.github.com>
2025-11-02 12:04:53 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-02T09:04:32.413Z
-Commit: Fix race condition in async operations
+Timestamp: 2025-11-02T09:04:53.424Z
+Commit: Fix CSS styling bug on mobile devices
